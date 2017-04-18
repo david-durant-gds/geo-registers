bd956db3e9f0b574fc7e028ceb5edcb57ff1f47b
David Durant <daviddurant@gds5200.local>
2017-04-18 23:02:44 +0100

    Many

--- web/library_functions.js
+++ web/library_functions.js
@@ -2,26 +2,61 @@
 
 var globalWidgetData;
 
-function pageStart(firstSelect) {
+function pageStart(element) {
   $.getJSON("global-widget-data.json", function(result){
     globalWidgetData = result;  
-    registerSelectOnChanges();
-    doOnChange(firstSelect);
+    loadData(element);
+
+//    registerSelectOnChanges();
+//    $("#" + inputElement).trigger( "change" );
   });
 }
+
+function loadSelectData(element) {
+  if ($("#" + element).is('select')) {
+
+    var jqxhr = $.get(element + ".txt")
+      .done(function(data) {
+
+console.log(this.id);
+
+/*
+        var $el = $("#" + element);
+        $el.empty();
+        splitData = data.split("\n");
+        $.each(splitData, function(loopNumber, value) {
+          $el.append($("<option></option>").attr("value", value).text(value));
+      });
+*/
+    })
+    .fail(function() {
+//      alert("Failed to load select : " + this.id);
+    });
+
+  }
+/*
+  else if (($("#" + element).is('textarea')) {
+  
+  
+  
+  }
+*/
+}
+
+
+
+
+
 
 function registerSelectOnChanges() {
 
   $('select').each(function() {
-    console.log(this.id);
+    this.change(doOnChange());
   });
 }
 
 function doOnChange(firstSelect) {
-}
 
-/*
-        $.each(result, function(i, field){
-            $("div").append(field + " ");
-        });
-*/
+  console.log(this.id);
+
+}
