e3c94cc74f3de089bf25e1d1b5e40b8d2adf92d4
David Durant <daviddurant@gds5200.local>
2017-04-19 02:50:36 +0100

    Many

--- web/library_functions.js
+++ web/library_functions.js
@@ -75,8 +75,10 @@ function doOnChange() {
 console.log(this.id);
 
   globalWidgetData.forEach(function(data) {
-    if ((data[0] == this.id) && (data[2] != "")) {
-      loadData(data[2]);
+    if (data[0] == this.id) {
+      if (data[2] != "")) {
+        loadData(data[2]);
+      }
     }
   });
 }
